ef87a0e1b21e4e43989937d47aaaf290725c3fdb
va-ovsyanikov <va.ovsyanikov@gmail.com>
2025-03-03 16:00:04 +0300

    Debounce

--- functions.ts
+++ functions.ts
@@ -1,3 +1,22 @@
+//Debounce
+
+export const  debounce = <T extends (...args: any[]) => void>(callee: T, timeoutMs: number) => {
+  let lastCall: number | null = null
+  let lastCallTimer: ReturnType<typeof setTimeout>
+
+  return function(this: any, ...args: Parameters<T>) {
+    const now = Date.now()
+
+    if (lastCall && now - lastCall <= timeoutMs) {
+      clearTimeout(lastCallTimer)
+    }
+
+    lastCall = now
+    lastCallTimer = setTimeout(() => callee.apply(this, args), timeoutMs)
+  }
+}
+
+
 //(Clamp)  Функции для подключения в SCSS, которые автоматически вычислят среднее значение
 /**
  * //Пример функции для размеров в rem
